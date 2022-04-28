8a360828f4916ebfec72b2d28acb5e7e67dfee94
Oren Levi <oren.stu@gmail.com>
2022-04-29 00:14:00 +0300

    optimize with tail-recursion but still too slow

--- problem-010/haskell/solution.hs
+++ problem-010/haskell/solution.hs
@@ -1,9 +1,17 @@
-sieve :: Int -> [Int]
-sieve n = go [2..(n-1)]
-  where
-    go :: [Int] -> [Int]
-    go [] = []
-    go (n:ns) = n : go [m | m <- ns, m `mod` n /= 0]
+-- ghc -funfolding-use-threshold=16 -O2 -optc-O3 solution.hs
 
--- too slow for 2M
-answer = sum $ sieve 10
+import System.Environment
+
+sieve :: Int -> [Int]
+sieve n = go [2..(n-1)] []
+  where
+    go :: [Int] -> [Int] -> [Int]
+    go [] res = res
+    go (n:ns) res = go [m | m <- ns, m `mod` n /= 0] (n:res)
+
+
+main = do
+  args <- getArgs
+  let nStr = head args
+  let n = read nStr :: Int
+  print $ sum $ sieve n
