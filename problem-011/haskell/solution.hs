1e972fbd2af149346be1562ff4fd3dad91ae6dcd
Oren Levi <orenl@yad2.co.il>
2024-08-20 14:43:09 +0300

    problem 011 - print each line separately

--- problem-011/haskell/solution.hs
+++ problem-011/haskell/solution.hs
@@ -1,16 +1,20 @@
 import System.IO
 import System.Environment
 import Data.List
+import Control.Monad
 
 main :: IO ()
 main = do
     args <- getArgs
     let filepath = head args
     contents <- readFile filepath
-    print $ toMatrix contents 
+    printEachLine $ toMatrix contents 
 
 toMatrix :: String -> [[Int]]
 toMatrix = map stringToInts . lines 
 
 stringToInts :: String -> [Int]
 stringToInts str = map read (words str) :: [Int]
+
+printEachLine :: Show a => [a] -> IO ()
+printEachLine = mapM_ print 
